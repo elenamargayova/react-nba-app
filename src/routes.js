6734472f5b6ec8cd8cbe2a831619ffbcedfab8b0
Elena <elenamargayova@gmail.com>
2018-06-12 11:59:28 +0300

    news and video main section is completed

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
 import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
+import NewsMain from './components/Articles/News/Main/index';
+import VideosMain from './components/Articles/Videos/Main/index';
 
 class Routes extends Component {
     render(){
@@ -12,8 +14,10 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/>
+                    <Route path="/news" exact component={NewsMain}/>
                     <Route path="/articles/:id" exact component={NewsArticle}/>
                     <Route path="/videos/:id" exact component={VideoArticle}/>
+                    <Route path="/videos" exact component={VideosMain}/>
                 </Switch>
             </Layout>
         )
