c7980e98390ae44af5b16c0030754f863bc62619
Elena <elenamargayova@gmail.com>
2018-06-12 11:08:55 +0300

    video post view is done.

--- src/components/Articles/Videos/Video/index.js
+++ src/components/Articles/Videos/Video/index.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 import {URL} from '../../../../config';
 import styles from '../../articles.css';
 import Header from './header';
+import VideosRelated from '../../../widgets/VideosList/VideosRelated/videosRelated';
 
 
 class VideoArticle extends Component {
     
     state={
         article:[],
-        team:[]
+        team:[],
+        teams:[],
+        related:[]
     }
 
     
@@ -24,6 +27,21 @@ class VideoArticle extends Component {
                     article,
                     team:response.data
                 })
+                this.getRelated();
+            })
+        })
+    }
+
+    getRelated = () => {
+        axios.get(`${URL}/teams`)
+        .then(response=>{
+            let teams=response.data
+            axios.get(`${URL}/videos?q=${this.state.team[0].city}&_limit=3`)
+            .then(response=>{
+                this.setState({
+                    teams,
+                    related:response.data
+                })
             })
         })
     }
@@ -37,9 +55,22 @@ class VideoArticle extends Component {
         return (
             <div>
                 <Header teamData={team[0]}/>
-                Video article
+                <div className={styles.videoWrapper}>
+                    <h1>{article.title}</h1>
+                    <iframe
+                        title="videoplayer"
+                        width="100%"
+                        height="300px"
+                        src={`https://www.youtube.com/embed/${article.url}`}
+                    >
+                    </iframe>
+                </div>
+                <VideosRelated
+                    data={this.state.related}
+                    teams={this.state.teams}
+                />
             </div>
-        );
+        )
     }
 }
 
